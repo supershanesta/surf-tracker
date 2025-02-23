4eb85b093f38f8b03461a0b9cf60411bdf1c0c45
Shane Rice <shanerice17@gmail.com>
2025-02-23 13:29:30 -0300

    adjust db urls

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -3,25 +3,36 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-	prisma = new PrismaClient({log: ['query', 'info', 'warn', 'error']});
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+    datasources: {
+      db: {
+        url: process.env.DIRECT_URL,
+      },
+    },
+  });
 } else {
-	// @ts-ignore
-	if (!global.prisma) {
-		// @ts-ignore
-		global.prisma = new PrismaClient({log: ['error']});
-	}
-	// @ts-ignore
-	prisma = global.prisma;
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ['error'],
+      datasources: {
+        db: {
+          url: process.env.DIRECT_URL,
+        },
+      },
+    });
+  }
+  prisma = global.prisma;
 }
 // @ts-ignore
-prisma.$on('query', (e) => { 
-	// @ts-ignore
-  console.log('Query: ' + e.query)
-	// @ts-ignore
-  console.log('Params: ' + e.params)
-	// @ts-ignore
-  console.log('Duration: ' + e.duration + 'ms')
-})
+prisma.$on('query', (e) => {
+  // @ts-ignore
+  console.log('Query: ' + e.query);
+  // @ts-ignore
+  console.log('Params: ' + e.params);
+  // @ts-ignore
+  console.log('Duration: ' + e.duration + 'ms');
+});
 
 export default prisma;
