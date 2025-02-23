2d02700d2278190aada625085fa02d8cc2bc3ab5
Shane Rice <shanerice17@gmail.com>
2025-02-23 13:36:11 -0300

    adjust configuration

--- scripts/setup-ec2.sh
+++ scripts/setup-ec2.sh
@@ -18,15 +18,20 @@ sudo apt install nginx -y
 sudo tee /etc/nginx/sites-available/surf-tracker << EOF
 server {
     listen 80;
-    server_name surf.shanedrice.com www.surf.shanedrice.com;  # Added www subdomain
+    server_name surf.shanedrice.com www.surf.shanedrice.com;
 
     location / {
+        # Reverse proxy for Next server
         proxy_pass http://localhost:3333;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
+        proxy_set_header Connection "upgrade";
         proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_set_header X-Forwarded-Port \$server_port;
     }
 }
 EOF
