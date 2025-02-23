16fbd3a84d3e629fdfbd695a99269bc40c3ed887
Shane Rice <shanerice17@gmail.com>
2025-02-23 14:03:51 -0300

    correct ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 3333,
         DATABASE_URL: process.env.DATABASE_URL,
+        DIRECT_URL: process.env.DIRECT_URL,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
       },
