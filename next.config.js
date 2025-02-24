d3d44aaea36e814bdf81c6a85c1b2870af8db9ba
Shane Rice <shanerice17@gmail.com>
2025-02-24 14:15:07 -0300

    correct url for backend calls

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/surf-session/",
-				permanent: true,
-			},
-		];
-	},
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/surf-session/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
